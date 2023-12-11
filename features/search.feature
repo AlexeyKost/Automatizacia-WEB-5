8e8fbfb5ad5bef0b9f0b366f44213f03b7c59730
Alexey <117473731+AlexeyKost@users.noreply.github.com>
2023-12-11 22:05:20 +0300

    Add files via upload

--- features/search.feature
+++ features/search.feature
@@ -1,5 +1,24 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+Feature: GoToTheCinema test
+    Scenario: Should book ticket
+        Given user is on "/client/index.php" page
+        When user choose day "3"
+        When user choose movie "1" and time "2"
+        When user choose row "6" and seat "5"
+        When user click book "button"
+        Then user sees booking confirmation "Вы выбрали билеты:"
+
+    Scenario: Should book  2 ticket
+        Given user is on "/client/index.php" page
+        When user choose day "5"
+        When user choose movie "2" and time "3"
+        When user choose row "1" and seat "7"
+        When user choose row "1" and seat "8"
+        When user click book "button"
+        Then user sees booking confirmation "Вы выбрали билеты:"
+
+    Scenario: Should not book ticket
+        Given user is on "/client/index.php" page
+        When user choose day "2"
+        When user choose movie "1" and time "3"
+        Then user sees the header "Зверополис"
+        Then user sees "button" is not clickable
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -1,8 +1,10 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
+const { clickElement, getText, chooseDay, chooseMovie, chooseSeat } = require("../../lib/commands.js");
+
+setDefaultTimeout(60000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -18,17 +20,52 @@ After(async function () {
 });
 
 Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php${string}`, {
     setTimeout: 20000,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user choose day {string}", { timeout: 60000 }, async function (string) {
+  return await chooseDay(this.page, string);
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
+When(
+  "user choose movie {string} and time {string}",
+  { timeout: 60000 },
+  async function (string, string2) {
+    return await chooseMovie(this.page, string, string2);
+  });
+
+When(
+  "user choose row {string} and seat {string}",
+  { timeout: 60000 },
+  async function (string, string2) {
+    return await chooseSeat(this.page, string, string2);
+  });
+
+When("user click book {string}", { timeout: 60000 }, async function (string) {
+  return await clickElement(this.page, string);
+});
+
+Then("user sees booking confirmation {string}", async function (string) {
+  const actual = await getText(this.page, "h2");
   const expected = await string;
   expect(actual).contains(expected);
 });
+
+Then("user sees the header {string}", async function (string) {
+  const actual = await getText(this.page, "h2");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Then(
+  "user sees {string} is not clickable",
+  { timeout: 60000 },
+  async function (string) {
+    const acceptionButton = await this.page.$eval(
+      "button",
+      (button) => button.disabled
+    );
+    await expect(acceptionButton).equal(true);
+  });
